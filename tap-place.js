0a599bc6d8f3b3ac7c9e2ca0f6800328540c6517
Yash Raj Bharti <yashrajbharti.met18@itbhu.ac.in>
2021-12-09 21:13:47 +0530

    tap to place ar

--- tap-place.js
+++ tap-place.js
@@ -10,18 +10,11 @@ const newElement = document.createElement('a-entity')
  console.log(touchPoint);
  newElement.setAttribute('position', touchPoint)
  newElement.setAttribute('visible', 'false')
- newElement.setAttribute('scale', '0.03 0.03 0.03')
- newElement.setAttribute('rotate', '0 0 0')
- newElement.setAttribute('geometry', {
-   primitive: 'ring',
-   radiusInner: 0.9,
-   radiusOuter: 1
- });
- newElement.setAttribute('material', 'color', 'red');
- newElement.setAttribute('material', 'side', 'double');
-
-  snaptarget.appendChild(newElement)
- newElement.setAttribute('position', '0.14 0.08 0')
+ newElement.setAttribute('scale', '0.01 0.01 0.01')
+ newElement.setAttribute('rotate', '0 0 90')
+ newElement.setAttribute('gltf-model', '#ring')
+ snaptarget.appendChild(newElement)
+ newElement.setAttribute('position', '0 -0.3 0')
 //  newElement.setAttribute('offset', '-455 -401 2975')
 
  newElement.addEventListener('model-loaded', () => {
