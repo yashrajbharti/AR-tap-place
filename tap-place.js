d8e429d3cd8684ee083ef80e8e95882f2de952ab
Yash Raj Bharti <yashrajbharti.met18@itbhu.ac.in>
2021-12-09 22:20:23 +0530

    tap to place ar

--- tap-place.js
+++ tap-place.js
@@ -21,8 +21,9 @@ const newElement = document.createElement('a-entity')
    newElement.setAttribute('material', 'side', 'double');
 
     snaptarget.appendChild(newElement)
-
-
+    touchPoint.x  /= 1000;
+    touchPoint.y  /= 1000;
+    touchPoint.z  /= 10000;
    newElement.setAttribute('position', touchPoint)
 
 
